f065782188c5adae28850eac725d6f038ec443a6
Linus-llm <linusmolgaard@gmail.com>
2026-04-09 17:09:06 +0200

    readme added

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,5 +1,4 @@
 package app;
-
 import app.Controller.CollectionController;
 import app.Controller.ItemController;
 import app.Controller.UserController;
@@ -8,12 +7,10 @@ import app.daos.BookDAO;
 import app.daos.CollectionDAO;
 import app.daos.ItemDAO;
 import app.daos.UserDAO;
-import app.entities.Book;
 import app.entities.Item;
 import app.entities.User;
 import app.security.Role;
 import app.security.SecurityController;
-import app.security.SecurityDao;
 import app.utils.BookService;
 import io.javalin.Javalin;
 import jakarta.persistence.EntityManagerFactory;
@@ -42,11 +39,8 @@ public class Main {
         //AUTH
         app.post("/api/auth/login", securityController::login);
         app.post("/api/auth/register", securityController::register);
-        app.get("/api/auth/protected", ctx-> ctx.json("test").status(200), Role.USER);
         //USER
         app.get("/api/user", userController::handleGetUsers, Role.USER);
-        //this endpoint is not needed since we have register, and we don't want users to create other users, so it's commented out
-        //app.post("/api/user", userController::handleCreateUser, Role.USER);
         app.get("/api/user/{id}", userController::handleGetUserById, Role.USER);
         app.put("/api/user/{id}", userController::handleUpdateUser, Role.USER);
         app.delete("/api/user/{id}", userController::handleDeleteUser, Role.USER);
@@ -67,12 +61,12 @@ public class Main {
         //BOOKS (item)
         app.post("/api/collection/{collectionId}/item", itemController::handleCreateBookInCollection,Role.USER);
 
-        //userDAO.create(new User("test@test.dk", "test", "testName"));
-        //BookService.saveBookChoiceToDatabase("Harry Potter", 1);
-        //for(Item i: itemDAO.getAll()){
-        //    System.out.println(i.name);
-        //}
-        //trigger
+        userDAO.create(new User("test@test.dk", "test", "testName"));
+        BookService.saveBookChoiceToDatabase("Harry Potter", 1);
+        for(Item i: itemDAO.getAll()){
+            System.out.println(i.name);
+        }
+
 
     }
 }
